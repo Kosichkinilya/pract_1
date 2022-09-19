25918f3ccf8f6b796eaa301b9dc8f08755bcb8b6
Kosichkinilya_work <-34@LAB44-08>
2022-09-19 18:08:39 +0300

    интерфейс

--- kosichkin 1 pr/MainWindow.xaml.cs
+++ kosichkin 1 pr/MainWindow.xaml.cs
@@ -25,20 +25,33 @@ namespace kosichkin_1_pr
             InitializeComponent();
         }
 
-        private void Exit_Click(object sender, RoutedEventArgs e)
+        private void Exit_Click(object sender, RoutedEventArgs e) //Выход
         {
             Close();
         }
 
-        private void Solve_Click(object sender, RoutedEventArgs e)
+        private void Mass_Click(object sender, RoutedEventArgs e) // Генерируем массив 
         {
-            MessageBox.Show("решить  ");
+
+            double[] array = new double[20];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rand.Next(-5, 5) + rand.NextDouble();
+            
         }
 
-        private void Info_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(" Найти сумму n целых, случайных чисел, распределенных в диапазоне от 0 до n. \r\nВывести на экран на одной строке сгенерированные числа, на другой строке \r\nрезультат");
-        }
 
+        private void Info_Click(object sender, RoutedEventArgs e) // Информация о приложении
+        {
+            MessageBox.Show
+                ("Сгенерировать массив заполненный случайными целыми числами," +
+                "\r\n распределенных в диапазоне от 0 до count," +
+                "\r\n где count - количество чисел в массиве." +
+                "\r\n Найти максимальное значение." +
+                "\r\n Вывести на экран на одной строке сгенерированные числа," +
+                "\r\n на другой строке результат." +
+                "\r\n В классе реализовать статический метод с именем Max.",
+                "\r\n Выполнил Косичкин Илья. Вариант 8");
+        }
     }
 }
