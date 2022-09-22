9fba908474973a0140954e58fdfbaeb7d678d066
Kosichkinilya_work <-34@LAB44-08>
2022-09-22 17:29:35 +0300

    доюаил класс

--- Class1/math.cs
+++ Class1/math.cs
@@ -1,10 +1,38 @@
 using System;
+using System.Windows.Navigation;
 
-namespace Class1
+namespace Lib_1
 {
-    public class math
+    public static class math
     {
+        public static int ArrayCreate(int[] numbers) // Генерируем массив но не выводим 
+        {
+            Random rnd = new();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(-10, 2);
 
+            }
+            return numbers.Length;
+        }
+
+        public static double SignSquare(int[] numbers) // вычисляем корень и возводим в квадрат, но пока не выводим 
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > 0)
+                {
+                    Math.Sqrt(numbers[i]);
+
+                }
+
+                if (numbers[i] < 0)
+                {
+                    Math.Pow(numbers[i], 2);
+                }
+            }
+            return numbers.Length;
+        }   
 
     }
 }
